b34d59de11aa121549c59788f4711814142c575c
tongjie <tongjieco@163.com>
2017-07-05 13:51:29 +0800

    modify

--- data/mainMenu.js
+++ data/mainMenu.js
@@ -1,15 +1,15 @@
 [
 	{
 		"typeName":"粉面系列",
-		"dataUrl":"data/subMenu_0"
+		"dataUrl":"data/subMenu_0.js"
 	},
 	{
 		"typeName":"米饭系列",
-		"dataUrl":"data/subMenu_1"
+		"dataUrl":"data/subMenu_1.js"
 	},
 	{
 		"typeName":"粥系列",
-		"dataUrl":"data/subMenu_2"
+		"dataUrl":"data/subMenu_2.js"
 	}
 ]
 
